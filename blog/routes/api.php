23dc4c763412457864944b00a37547105c38c44a
HunterDead14 <mykola.ilchuk@oa.edu.ua>
2026-05-24 23:25:19 +0300

    Lab 3: controllers

--- blog/routes/api.php
+++ blog/routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['namespace' => 'App\Http\Controllers\Api\Blog', 'prefix' => 'blog'], function () {
+    Route::apiResource('posts', PostController::class)->names('blog.posts');
+});
