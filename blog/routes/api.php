bb8a17f9824f1cec6188d8158c04224511603b91
HunterDead14 <mykola.ilchuk@oa.edu.ua>
2026-05-28 13:37:43 +0300

    Lab 8: post

--- blog/routes/api.php
+++ blog/routes/api.php
@@ -22,4 +22,10 @@ Route::group($groupData, function () {
     Route::apiResource('categories', CategoryController::class)
         ->only($methods)
         ->names('blog.admin.categories');
+    //BlogPost
+    Route::apiResource('posts', PostController::class)
+        ->except(['show'])                               //не робити маршрут для метода show
+        ->names('blog.admin.posts');
 });
+
+
