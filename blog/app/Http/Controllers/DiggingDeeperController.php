741cca80ae7e224d2c62860112bacc0676c22eba
HunterDead14 <mykola.ilchuk@oa.edu.ua>
2026-06-09 12:15:52 +0300

    Lab 13:Ланцюги завдань (Chain)

--- blog/app/Http/Controllers/DiggingDeeperController.php
+++ blog/app/Http/Controllers/DiggingDeeperController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\BlogPost;
 use Carbon\Carbon;
+use App\Jobs\ProcessVideoJob;
+use App\Jobs\GenerateCatalog\GenerateCatalogMainJob;
+
 
 class DiggingDeeperController extends Controller
 {
@@ -18,6 +21,28 @@ class DiggingDeeperController extends Controller
      * @url https://laravel.com/api/13.x/Illuminate/Database/Eloquent/Collection.html
      *
      */
+
+    public function processVideo()
+    {
+        ProcessVideoJob::dispatch();
+        // Відкладення виконання завдання від моменту потрапляння в чергу.
+        // Не впливає на паузу між спробами виконання завдання.
+        //->delay(10)
+        //->onQueue('name_of_queue')
+    }
+
+    /**
+     * @link http://localhost:8000/digging_deeper/prepare-catalog
+     *
+     * php artisan queue:listen --queue=generate-catalog --tries=3 --delay=10
+     */
+    public function prepareCatalog()
+    {
+        GenerateCatalogMainJob::dispatch();
+    }
+
+
+
 
     public function collections()
     {
@@ -123,6 +148,9 @@ class DiggingDeeperController extends Controller
         $sortedDescCollection = $collection->sortByDesc('item_id');
 
         //dd(compact('sortedSimpleCollection', 'sortedAscCollection', 'sortedDescCollection'));
+
+
+
 
     }
 }
